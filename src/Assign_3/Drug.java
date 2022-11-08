5182fe3e177a602bbee5d9c53a311dffab906361
Avandj <ivanalc.oviedo@gmail.com>
2022-11-08 15:36:52 -0500

    commit

--- src/Assign_3/Drug.java
+++ src/Assign_3/Drug.java
@@ -1,4 +1,65 @@
 package Assign_3;
 
 public class Drug {
+
+    private String   GenericName;
+    private String   SMILES;
+    private String   drugBankID;
+    private String   url;
+    private String   drugGroups;
+    private double      score;
+
+    public Drug(String GenericName,String SMILES,String drugBankID,String url,String drugGroups,double score){
+
+        this.GenericName=GenericName;
+        this.SMILES=SMILES;
+        this.drugBankID=drugBankID;
+        this.url=url;
+        this.drugGroups=drugGroups;
+        this.score=score;
+
+    }
+
+
+    public String getGenericName() {
+        return GenericName;
+    }
+
+    public String getSMILES() {
+        return SMILES;
+    }
+
+    public int getID(){
+        int key;
+        String temp="";
+
+        for (int i=2; i<drugBankID.length();i++){
+
+            temp+=drugBankID.charAt(i);
+
+        }
+        key= Integer.parseInt(temp);
+        return key;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getDrugGroups() {
+        return drugGroups;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void displayDrug(){
+        System.out.println(GenericName+"\t"+SMILES+"\t"+drugBankID+"\t"+url+"\t"+drugGroups+"\t"+score);
+    }
+
+    public String returnDrug(){
+        return (GenericName+"\t"+SMILES+"\t"+drugBankID+"\t"+url+"\t"+drugGroups+"\t"+score);
+    }
+
 }
--- src/Assign_3/DrugHeap.java
+++ src/Assign_3/DrugHeap.java
@@ -1,4 +1,64 @@
 package Assign_3;
+import java.io.*;
 
 public class DrugHeap {
+
+    static BufferedReader daReader = null;
+    public static Drug [] mH = new Drug[1932]; //Stand for min-Heap and stores the min heap in an array representation
+    public static BufferedWriter daWriter;
+
+    public static String[] []data = new String[1932][6]; //1932
+
+    DrugHeap() throws IOException{
+        daReader=null;
+        readData();
+    }
+
+
+
+
+
+
+
+
+    private static void readData() throws IOException {
+
+        int num=0;
+        int collumn=0;
+
+        daReader = new BufferedReader(new FileReader("dockedApproved.tab"));
+
+        String temp = null;
+        String[] temp2 =new String[6];
+        daReader.readLine();
+        do{
+            temp = daReader.readLine();
+
+            if (temp!=null){
+                temp2=temp.split("\t");
+                for(int i=0;i<6;i++){
+                    data[num][i] = temp2[i];
+                }
+                num++;
+            }
+
+        }while(temp!=null);
+
+
+
+        /*
+         for (int i=0;i< data.length;i++){
+             for (int j=0; j<6;j++){
+                 System.out.print(data[i][j]+"\t");
+             }
+             System.out.println();
+
+         }
+
+         */
+
+
+
+
+    }
 }
